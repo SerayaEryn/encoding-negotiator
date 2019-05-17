e92bb157b943a6541a6efd654b87076851072777
Denis Fäcke <serayaeryn@gmail.com>
2019-05-17 17:10:07 +0200

    replace tap with ava

--- test/test.js
+++ test/test.js
@@ -1,94 +1,84 @@
 'use strict'
 
-const t = require('tap')
-const test = t.test
+const test = require('ava')
 const enodingNegotiator = require('..')
 
 test('should return identity', (t) => {
-  t.plan(1)
   const header = 'identity;q=1'
   const supported = ['gzip']
 
   const result = enodingNegotiator.negotiate(header, supported)
 
-  t.strictEquals(result, 'identity')
+  t.is(result, 'identity')
 })
 
-test('should return identity', (t) => {
-  t.plan(1)
+test('should return identity if header is undefined', (t) => {
   const supported = ['gzip']
 
   const result = enodingNegotiator.negotiate(undefined, supported)
 
-  t.strictEquals(result, 'identity')
+  t.is(result, 'identity')
 })
 
 test('should return gzip', (t) => {
-  t.plan(1)
   const header = 'gzip;q=1, identity;q=0.5'
   const supported = ['gzip', 'deflate']
 
   const result = enodingNegotiator.negotiate(header, supported)
 
-  t.strictEquals(result, 'gzip')
+  t.is(result, 'gzip')
 })
 
-test('should return gzip', (t) => {
-  t.plan(1)
+test('should return deflate', (t) => {
   const header = 'deflate;q=0.5,identity; q=0.5'
   const supported = ['gzip', 'deflate']
 
   const result = enodingNegotiator.negotiate(header, supported)
 
-  t.strictEquals(result, 'deflate')
+  t.is(result, 'deflate')
 })
 
 test('"*" and ["gzip", "deflate"]', (t) => {
-  t.plan(1)
   const header = '*'
   const supported = ['gzip', 'deflate']
 
   const result = enodingNegotiator.negotiate(header, supported)
 
-  t.strictEquals(result, 'gzip')
+  t.is(result, 'gzip')
 })
 
 test('"deflate;q=1.0, *" and ["gzip"]', (t) => {
-  t.plan(1)
   const header = 'deflate;q=1.0, *'
   const supported = ['gzip']
 
   const result = enodingNegotiator.negotiate(header, supported)
 
-  t.strictEquals(result, 'gzip')
+  t.is(result, 'gzip')
 })
 
 test('"gzip;q=0" and ["gzip"]', (t) => {
-  t.plan(1)
   const header = 'gzip;q=0'
   const supported = ['gzip']
 
   const result = enodingNegotiator.negotiate(header, supported)
 
-  t.strictEquals(result, 'identity')
+  t.is(result, 'identity')
 })
 
 test('compress;q=0.5, gzip;q=1.0 and ["gzip", compress"]', (t) => {
-  t.plan(1)
   const header = 'compress;q=0.5, gzip;q=1.0'
   const supported = ['gzip', 'compress']
 
   const result = enodingNegotiator.negotiate(header, supported)
 
-  t.strictEquals(result, 'gzip')
+  t.is(result, 'gzip')
 })
 
 test('compress;q=0.5, gzip;q=1.0 and ["compress"]', (t) => {
-  t.plan(1)
   const header = 'compress;q=0.5, gzip;q=1.0'
   const supported = ['compress']
 
   const result = enodingNegotiator.negotiate(header, supported)
 
-  t.strictEquals(result, 'compress')
+  t.is(result, 'compress')
 })
